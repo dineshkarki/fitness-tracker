e758baf67dc2c8b54c3ae72da8f83d1ec29f3488
Dineshkumar Karki <Dineshkumar.Karki@gisbizdev.gisbiz.chennai.com>
2019-12-24 19:43:11 +0530

    first commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule} from '@angular/material';
+
+@NgModule({
+    imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule]
+})
+
+export class MaterialModule {}
